54e01225f130e8cd13581415c861cddba7ff3072
Eliascolin <eliascolina.c@gmail.com>
2023-03-09 21:10:03 -0300

    finalizado

--- aula 9/aula9.js
+++ aula 9/aula9.js
@@ -14,7 +14,6 @@ function microondas (comida,tempo){
     let prato = 0
 
     switch (comida){
-
         case "pipoca": prato = 10
         break
         case "macarrao": prato = 8
@@ -24,44 +23,34 @@ function microondas (comida,tempo){
          case "feijao": prato = 12
          break
          case "brigadeiro": prato = 8
-         break;
+         break
          default: prato = 0
          break
-         
-         
-
-
-
-    };
-
-
-
+    }
 //Opções não listadas no menu, devem exibir uma mensagem de erro: "Prato inexistente";
 if(prato == 0) (
     console.log("prato nao existe")
 )
 //Se o tempo for menor que o padrão, exibir a mensagem: "tempo insuficiente";
 
- else if(tempo < comida ){
+ else if(tempo < prato ){
     console.log("tempo insuficiente")
 }
-
 //Se o tempo informado for maior que 2x o necessário, exibir mensagem que a comida queimou.
-
-else if(tempo > comida*2 && tempo < comida*3){
+else if(tempo > prato*2 && tempo < prato*3){
     console.log('queimou')
 }
 
 //Se o tempo for 3x maior que o necessário para o prato, o microondas deve exibir a mensagem: “kabumm”
 
-else if(tempo >= comida*3){
-    console.log('queimou')
+else if(tempo >= prato*3){
+    console.log('kabum')
 }
 
 else(console.log("prato feito"))
 };
 
-microondas('carne',19)
+microondas('arepa',31)
 
 //  pipoca = 10
 //  macarrao = 8
